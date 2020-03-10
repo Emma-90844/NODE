2c4926b2c46db64653f16fc3a02212803483b009
Emma <enabojohn@gmail.com>
2020-03-10 18:56:04 +0300

    Innitial commit , (The global object)

---
+++ global.js
@@ -0,0 +1,35 @@
+
+var path = require('path')
+var name = "Edwin";
+var newName = name.toUpperCase(name);
+console.log(newName)
+
+console.log(__dirname);
+console.log(__filename);
+
+
+
+console.log(path.basename(__filename));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
